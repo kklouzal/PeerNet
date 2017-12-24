f3bfb094a2d5b3cfe1ffc17f2bf4caf3c3e4746c
KKlouzal <kyle.klouzal@gmail.com>
2017-12-24 14:33:32 -0700

    Visual Studio is a PITA sometimes.

---
+++ PeerNet/PeerNet.cpp
@@ -0,0 +1,4 @@
+#include "PeerNet.hpp"
+//	This file is only present to allow Visual Studio to accurately code-complete this header-only-library
+//	Actual use of the library does not require this file or require direct compilation of PeerNet
+//	Simply #include PeerNet.hpp and point your projects additional header include directory to the appropriate folder
